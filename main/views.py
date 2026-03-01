697f2c338535cfe3587700e0f67311bb2129d759
Khadim Fall <khadimfall0503@gmail.com>
2026-03-01 18:41:38 -0500

    Modification du fichier index

--- main/views.py
+++ main/views.py
@@ -1,34 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, ListView
-from .models import Specialite, Service, Medecin
+from .models import Service, Medecin
 
-# Create your views here.
+
 class IndexView(TemplateView):
     template_name = 'main/index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['specialites'] = Specialite.objects.all()  # récupère toutes les spécialités
+        context['services'] = Service.objects.order_by('-id')[:4]  # 4 services récents
         return context
-    
-class SpecialitesView(ListView):
-    model = Specialite.objects.all()
-    template_name = 'main/acceuil_spe.html'
-    context_object_name = 'specialites'
-    
+
+
 class ServicesView(ListView):
     model = Service
     template_name = 'main/services.html'
     context_object_name = 'services'
-    
+    ordering = ['-id']
+
+
 class ServiceDetailView(TemplateView):
     template_name = 'main/service_detail.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         service_id = self.kwargs.get('pk')
-        context['service'] = Service.objects.get(id=service_id)
+        context['service'] = get_object_or_404(Service, id=service_id)
         return context
+
+
 class MedecinsView(ListView):
     model = Medecin
     template_name = 'main/medecins.html'
